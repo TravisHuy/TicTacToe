17de20c5ff97ec0158021958ead5b5806ed5ddac
TravisHuy <honhathuy098@gmail.com>
2024-06-26 18:24:23 +0700

    update ui

--- app/src/main/java/com/nhathuy/tictactoe/ui/GameInfo.kt
+++ app/src/main/java/com/nhathuy/tictactoe/ui/GameInfo.kt
@@ -1,10 +1,42 @@
 package com.nhathuy.tictactoe.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.nhathuy.tictactoe.intent.TicTacToeIntent
 import com.nhathuy.tictactoe.model.TicTacToeState
 
 @Composable
 fun GameInfo(state: TicTacToeState, onIntent: (TicTacToeIntent)-> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Current Player:  ${ state.currentPlayer }",
+            style = MaterialTheme.typography.headlineSmall)
+        Spacer(modifier = Modifier.height(16.dp))
+        when{
+        state.winner !=null ->
+            Text(text = "Winner: ${state.winner}",
+            style = MaterialTheme.typography.headlineMedium)
+
+            state.isDraw -> Text(
+                text = "It's a draw ",
+                style = MaterialTheme.typography.headlineMedium)
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { onIntent(TicTacToeIntent.ResetGame) }) {
+            Text(text = "Reset Game")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = { onIntent(TicTacToeIntent.BackToMenu) }) {
+            Text(text = "Back to Menu")
+        }
+    }
 
 }
