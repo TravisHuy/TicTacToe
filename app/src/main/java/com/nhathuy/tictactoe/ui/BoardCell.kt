3546f3b819a60f080431cb9e2daf0c1fe6bf2d5a
TravisHuy <honhathuy098@gmail.com>
2024-06-28 09:42:15 +0700

    update interface

--- app/src/main/java/com/nhathuy/tictactoe/ui/BoardCell.kt
+++ app/src/main/java/com/nhathuy/tictactoe/ui/BoardCell.kt
@@ -1,25 +1,48 @@
 package com.nhathuy.tictactoe.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun BoardCell(value: String?, onClick: () -> Unit, enabled: Boolean) {
-    Button(onClick = onClick,
+    Box(
         modifier = Modifier
-            .size(100.dp)
-            .padding(4.dp),
-        enabled=enabled) {
+            .fillMaxSize()
+            .padding(4.dp)
+            .background(Color.Transparent)
+            .clickable(
+                enabled = enabled,
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = onClick
+            ),
+            contentAlignment = Alignment.Center) {
         Text(text = value ?: "",
-        style = MaterialTheme.typography.headlineMedium)
+        style = MaterialTheme.typography.headlineLarge.copy(
+            fontFamily = FontFamily.Cursive,
+            fontWeight = FontWeight.Bold,
+            fontSize = 48.sp,
+            color = if(value=="X") Color.Blue else Color.Red
+        ))
 
     }
 }
--- app/src/main/java/com/nhathuy/tictactoe/ui/GameBoard.kt
+++ app/src/main/java/com/nhathuy/tictactoe/ui/GameBoard.kt
@@ -1,30 +1,97 @@
 package com.nhathuy.tictactoe.ui
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.unit.dp
 import com.nhathuy.tictactoe.intent.TicTacToeIntent
 import com.nhathuy.tictactoe.model.TicTacToeState
 
 @Composable
 fun GameBoard(state: TicTacToeState, onIntent: (TicTacToeIntent) ->  Unit) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        for (i in 0..2){
-            Row {
-                for (j in 0..2){
-                    key(i*3+j) {
-                        BoardCell(
-                            value= state.board[i][j],
-                            onClick = {
-                                onIntent(TicTacToeIntent.CellClicked(i,j))
-                            },
-                            enabled= state.board[i][j]==null && state.winner == null && !state.isDraw
-                        )
+
+    val navyBlue= Color(0xFF000080)
+    Surface(modifier = Modifier
+        .aspectRatio(1f)
+        .padding(16.dp),
+        shape = RoundedCornerShape(8.dp),
+        color = Color.White,
+        shadowElevation = 4.dp
+    ) {
+        Box{
+            Canvas(modifier = Modifier.fillMaxSize()){
+                val strokeWidth= 2.dp.toPx()
+
+                drawLine(
+                    color = navyBlue,
+                    start = Offset(size.width / 3, 0f),
+                    end = Offset(size.width / 3, size.height),
+                    strokeWidth = strokeWidth,
+                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                )
+                drawLine(
+                    color = navyBlue,
+                    start = Offset(2*size.width / 3, 0f),
+                    end = Offset(2*size.width / 3, size.height),
+                    strokeWidth = strokeWidth,
+                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                )
+                drawLine(
+                    color = navyBlue,
+                    start = Offset(0f, size.height/3),
+                    end = Offset(size.width , size.height/ 3),
+                    strokeWidth = strokeWidth,
+                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                )
+                drawLine(
+                    color = navyBlue,
+                    start = Offset(0f, 2*size.height/3),
+                    end = Offset(size.width,2*size.height/3),
+                    strokeWidth = strokeWidth,
+                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                )
+            }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                for (i in 0..2) {
+                    Row(
+                        modifier = Modifier.weight(1f),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        for (j in 0..2) {
+                            Box(modifier = Modifier.weight(1f)) {
+                                BoardCell(
+                                    value = state.board[i][j],
+                                    onClick = {
+                                        onIntent(TicTacToeIntent.CellClicked(i, j))
+                                    },
+                                    enabled = state.board[i][j] == null && state.winner == null && !state.isDraw
+                                )
+                            }
+                        }
                     }
                 }
             }
+
+
         }
     }
 }
